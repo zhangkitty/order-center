c7bbd7f6eea9ef1f5521fbb1ad429cafb651e8ce
liufeng <liufeng@dotfashion.cn>
2017-09-19 15:05:03 +0800

    退款列表--

--- src/components/refund/list/types.js
+++ src/components/refund/list/types.js
@@ -4,131 +4,15 @@
 
 const prefix = 'refund_';
 export const INIT = `${prefix}init`;
-export const OPEN_MODAL = `${prefix}open_modal`;
-export const OPEN_MODAL_CGS = `${prefix}open_modal_cgs`;
 
 export const SEARCH = `${prefix}search`;
 export const SEARCH_SUCCESS = `${prefix}search_success`;
 export const SEARCH_FAIL = `${prefix}search_fail`;
 
-export const SEARCH_HIGH = `${prefix}search_high`;
-export const SEARCH_HIGH_SUCCESS = `${prefix}search_high_success`;
-export const SEARCH_HIGH_FAIL = `${prefix}search_high_fail`;
-
-// history order
-export const SEARCH_HISTORY = `${prefix}search_history`;
-export const SEARCH_HISTORY_SUCCESS = `${prefix}search_history_success`;
-export const SEARCH_HISTORY_FAIL = `${prefix}search_history_fail`;
-
 export const CHANGE = `${prefix}change`;
 export const COMMIT = `${prefix}commit`;
-export const COMMIT_HIGH = `${prefix}commit2`;
-export const COMMIT3 = `${prefix}commit3`;
+
 
 export const INIT_COUNTRY = `${prefix}init_country`;
 export const INIT_COUNTRY_SUCCESS = `${prefix}init_country_success`;
 export const INIT_COUNTRY_FAIL = `${prefix}init_country_fail`;
-
-export const INIT_SITE = `${prefix}init_site`;
-export const INIT_SITE_SUCCESS = `${prefix}init_site_success`;
-export const INIT_SITE_FAIL = `${prefix}init_site_fail`;
-
-// 支付方式
-export const INIT_PAYMENT = `${prefix}init_payment`;
-export const INIT_PAYMENT_SUCCESS = `${prefix}init_payment_success`;
-export const INIT_PAYMENT_FAIL = `${prefix}init_payment_fail`;
-
-// 问题件类型
-export const INIT_TROUBLE = `${prefix}init_trouble`;
-export const INIT_TROUBLE_SUCCESS = `${prefix}init_trouble_success`;
-export const INIT_TROUBLE_FAIL = `${prefix}init_trouble_fail`;
-
-// 会员等级
-export const INIT_MEMBER = `${prefix}init_member`;
-export const INIT_MEMBER_SUCCESS = `${prefix}init_member_success`;
-export const INIT_MEMBER_FAIL = `${prefix}init_member_fail`;
-
-// 订单状态
-export const INIT_ORDER = `${prefix}init_order`;
-export const INIT_ORDER_SUCCESS = `${prefix}init_order_success`;
-export const INIT_ORDER_FAIL = `${prefix}init_order_fail`;
-
-// 取消类型
-export const INIT_CANCEL = `${prefix}init_cancel`;
-export const INIT_CANCEL_SUCCESS = `${prefix}init_cancel_success`;
-export const INIT_CANCEL_FAIL = `${prefix}init_cancel_fail`;
-
-// 商品状态
-export const INIT_GOODS = `${prefix}init_goods`;
-export const INIT_GOODS_SUCCESS = `${prefix}init_goods_success`;
-export const INIT_GOODS_FAIL = `${prefix}init_goods_fail`;
-
-// 商品操作查询
-export const OPERATION_GOODS = `${prefix}operation_goods`;
-export const OPERATION_GOODS_SUCCESS = `${prefix}operation_goods_success`;
-export const OPERATION_GOODS_FAIL = `${prefix}operation_goods_fail`;
-
-// 备注显示
-export const REMARK = `${prefix}remark`;
-export const REMARK_SUCCESS = `${prefix}remark_success`;
-export const REMARK_FAIL = `${prefix}remark_fail`;
-
-// 备注更新
-export const REMARK_SAVE = `${prefix}remark_save`;
-export const REMARK_SAVE_SUCCESS = `${prefix}remark_save_success`;
-export const REMARK_SAVE_FAIL = `${prefix}remark_save_fail`;
-
-// 物流备注显示
-export const LOGISITICS_REMARK = `${prefix}logistics_remark`;
-export const LOGISITICS_REMARK_SUCCESS = `${prefix}logistics_remark_success`;
-export const LOGISITICS_REMARK_FAIL = `${prefix}logistics_remark_fail`;
-
-// 物流备注更新
-export const LOGISITICS_REMARK_SAVE = `${prefix}logistics_remark_save`;
-export const LOGISITICS_REMARK_SAVE_SUCCESS = `${prefix}logistics_remark_save_success`;
-export const LOGISITICS_REMARK_SAVE_FAIL = `${prefix}logistics_remark_save_fail`;
-
-
-// sku查size
-export const GOODS_SIZE = `${prefix}goods_size`;
-export const GOODS_SIZE_SUCCESS = `${prefix}goods_size_success`;
-export const GOODS_SIZE_FAIL = `${prefix}goods_size_fail`;
-
-// 换货
-export const CHANGE_GOODS = `${prefix}change_goods`;
-export const CHANGE_GOODS_SUCCESS = `${prefix}change_goods_success`;
-export const CHANGE_GOODS_FAIL = `${prefix}change_goods_fail`;
-
-// 删除换货
-export const DEL_CHANGE = `${prefix}del_change`;
-export const DEL_CHANGE_SUCCESS = `${prefix}del_change_success`;
-export const DEL_CHANGE_FAIL = `${prefix}del_change_fail`;
-
-// 批量操作
-export const BATCH_OPERATE = `${prefix}batch_operate`;
-export const BATCH_OPERATE_SUCCESS = `${prefix}batch_operate_success`;
-
-// 批量审核
-export const BATCH_CHECK = `${prefix}batch_check`;
-export const BATCH_CHECK_SUCCESS = `${prefix}batch_check_success`;
-
-// 批量平台取消
-export const BATCH_DELETE = `${prefix}batch_delete`;
-export const BATCH_DELETE_SUCCESS = `${prefix}batch_delete_success`;
-
-// 批量部分发
-export const BATCH_PART = `${prefix}batch_part`;
-export const BATCH_PART_SUCCESS = `${prefix}batch_part_success`;
-
-// 取消风控
-export const CANCEL_RISK = `${prefix}cancel_risk`;
-export const CANCEL_RISK_SUCCESS = `${prefix}cancel_risk_success`;
-
-// 取消问题订单标签
-export const CANCEL_TROUBLE_TAG = `${prefix}cancel_trouble_tag`;
-export const CANCEL_TROUBLE_TAG_SUCCESS = `${prefix}cancel_trouble_tag_success`;
-
-// 更新订单标签
-export const MARK_TAG = `${prefix}mark_tag`;
-export const UPDATE_ORDER_TAG = `${prefix}update_mark_tag`;
-export const UPDATE_ORDER_TAG_SUCCESS = `${prefix}update_mark_tag_success`;
