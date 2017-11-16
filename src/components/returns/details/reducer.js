66ca4ef9f421f619e00136209944075623161098
liufeng <liufeng@dotfashion.cn>
2017-11-16 15:08:26 +0800

    退货单 详情

--- src/components/returns/details/reducer.js
+++ src/components/returns/details/reducer.js
@@ -3,17 +3,19 @@ import * as TYPES from './types';
 
 const defaultState = {
   ready: false,
-  returnsInfoReady:false,
-  returnsInfoData:null,
+  returnsInfoReady: false,
+  returnsInfoData: null,
+  tracking_no_url: null,
 };
 
 export default (state = defaultState, action) => {
   switch (action.type) {
     case TYPES.GETORDERRETURNDETAILSUCCESS:
-      return assign({},state,{
-        returnsInfoReady:true,
-        returnsInfoData:action.data,
-      })
+      return assign({}, state, {
+        returnsInfoReady: true,
+        returnsInfoData: action.data,
+        tracking_no_url: action.data.trackingNoUrl,
+      });
     default:
       return state;
   }
