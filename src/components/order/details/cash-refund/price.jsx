b3a004714c7803ba3c7ef79984ae08f7f6fad8d4
zhangcaochao23 <zhangcaochao@shein.com>
2017-11-23 10:56:29 +0800

    修改isUsd

--- src/components/order/details/cash-refund/price.jsx
+++ src/components/order/details/cash-refund/price.jsx
@@ -13,47 +13,45 @@ import style from './style.css';
 
 const star = (<span style={{ color: 'red' }}>*</span>);
 
-const Price = ({ submitValue, dispatch }) => {
-  return (
-    <div className={style.space}>
-      <span className={style.descWidth}>{__('order.entry.cash_content5')}{star}:</span>
+const Price = ({ submitValue, dispatch }) => (
+  <div className={style.space}>
+    <span className={style.descWidth}>{__('order.entry.cash_content5')}{star}:</span>
 
-      <div className={style.flex_center}>
-        <span className={style.flex_span}>$</span>
-        <div>
-          <Input
-            type="number"
-            className={style.flex_input}
-            value={submitValue.refundAmount}
-            disabled
-            onChange={(e) => {
-              dispatch(subchange('refundAmount', e.target.value));
+    <div className={style.flex_center}>
+      <span className={style.flex_span}>$</span>
+      <div>
+        <Input
+          type="number"
+          className={style.flex_input}
+          value={submitValue.refundAmount}
+          disabled
+          onChange={(e) => {
+            dispatch(subchange('refundAmount', e.target.value));
             //  dispatch(subchange('refundCurrency', Number(Number(e.target.value) * submitValue.rate2).toFixed(2))); // 取消，美元填写
-            }}
-          />
-          <span
-            className={style.flex_span}
-            dangerouslySetInnerHTML={{ __html: submitValue.currency }}
-          />
-          <Input
-            type="number"
-            className={style.flex_input}
-            value={submitValue.refundCurrency}
-            onChange={(e) => {
-              dispatch(subchange('refundAmount', Number(Number(e.target.value) / submitValue.rate2).toFixed(2)));
-              dispatch(subchange('refundCurrency', e.target.value));
-            }}
-          />
-          <span className={style.tipStyle}>
-            {__('order.goodsRefund.no_over_price')}
-            {submitValue.currency}
-            { Number(submitValue.max).toFixed(2) }
-          </span>
-        </div>
+          }}
+        />
+        <span
+          className={style.flex_span}
+          dangerouslySetInnerHTML={{ __html: submitValue.currency }}
+        />
+        <Input
+          type="number"
+          className={style.flex_input}
+          value={submitValue.refundCurrency}
+          onChange={(e) => {
+            dispatch(subchange('refundAmount', Number(Number(e.target.value) / submitValue.rate2).toFixed(2)));
+            dispatch(subchange('refundCurrency', e.target.value));
+          }}
+        />
+        <span className={style.tipStyle}>
+          {__('order.goodsRefund.no_over_price')}
+          {submitValue.currency}
+          { Number(submitValue.max).toFixed(2) }
+        </span>
       </div>
     </div>
+  </div>
   );
-};
 
 Price.propTypes = {
   submitValue: PropTypes.shape(),
