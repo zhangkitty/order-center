f41cabd5817ccbd24b9d03608e719de44b11ca29
liufeng <fox.liu@shein.com>
2018-03-09 11:53:59 +0800

    47553 （bug 修复，账户和 卡号）

--- src/components/order/goodsRefund/price.jsx
+++ src/components/order/goodsRefund/price.jsx
@@ -190,7 +190,7 @@ const Price = ({ dataSource, submitValue, dispatch, isUsd }) => {
                           <Input
                             placeholder={__('order.entry.cash_content11')} // 银行卡号
                             style={{ width: 150, marginLeft: '5px' }}
-                            value={v.account}
+                            value={v.account1}
                             onChange={(e) => {
                               if (/\s/.test(e.target.value)) { return false; }   // 不允许空格
                               return onC(dispatch, submitValue, i, {
