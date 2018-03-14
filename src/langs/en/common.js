0006d922cf34164552363bdf55aa4eb4dc96bc01
wangyongxv <niko.wang@shein.com>
2018-03-14 11:42:36 +0800

    add some change

--- src/langs/en/common.js
+++ src/langs/en/common.js
@@ -42,7 +42,7 @@ module.exports = {
   guangzhou: 'Guangzhou warehouse',
   west: 'Western warehouse',
   nansha: 'nansha warehouse',
-  america: 'Eastern United States warehouse',
+  america: 'Eastern America warehouse',
   europe: 'Europe warehouse',
   platform: 'Platform Order cancelled',
   review: 'Bulk confirmation',
