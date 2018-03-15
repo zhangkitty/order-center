643bd90f193de0a75bdc523339b5ce9ed39603f9
wangyongxv <niko.wang@shein.com>
2018-03-15 12:00:44 +0800

    add some change

--- src/components/order/details/track-details/saga.js
+++ src/components/order/details/track-details/saga.js
@@ -1,14 +1,24 @@
 import { takeLatest, put } from 'redux-saga/effects';
+import { message } from 'antd';
 import * as Types from './types';
 import { getInitDataServer } from '../server';
 import { putData } from './action';
 
+
+const lan = {
+  dataFail: __('order.entry.submit_info2'), // 获取数据失败
+};
+
 function* getInitDataSaga({ id }) {
   const result = yield getInitDataServer(id);
   if (result.code === 0) {
-    yield put(putData(result.data));
+    if (result.data.length > 0) {
+      yield put(putData(result.data));
+      return;
+    }
+    window.location = `https://track.trackingmore.com/choose-cn-${id}.html`;
   } else {
-    //  window.location = `https://track.trackingmore.com/choose-cn-${id}.html`;
+    message.error(`${lan.dataFail}:${result.msg}`);
   }
 }
 
