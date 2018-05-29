38716fc6dc6ad269683a9984af7412dc9f5c381c
zhangcaochao <zhangcaochao30@gmail.com>
2018-05-29 14:16:17 +0800

    104

--- src/components/bulk-processing-refund/view.jsx
+++ src/components/bulk-processing-refund/view.jsx
@@ -7,13 +7,14 @@ const lan = {
   批量上传物流原因退款: '批量上传物流原因退款',
   批量上传投诉订单退款: '批量上传投诉订单退款',
   下载样例: '下载样例',
+  错误信息: '错误信息',
+  正确信息: '正确信息',
 };
 export default class Order extends Component {
   state = {
-    value1: {},
-    value2: {},
+    value1: [],
+    value2: [],
   };
-
 
   render() {
     const setProps1 = {
@@ -27,13 +28,21 @@ export default class Order extends Component {
       onChange: (info) => {
         if (info.file.response !== undefined) {
           if (info.file.response.code === 0) {
+            const temp1 = this.state.value1;
+            const temp2 = this.state.value2;
+            temp1.push(info.file.response.data.error);
+            temp2.push(info.file.response.data.log);
+
             this.setState({
-              value1: info.file.response.data,
-              value2: this.state.value2,
+              value1: temp1,
+              value2: temp2,
             });
-            message.success(info.file.response.msg);
           } else {
-            message.error(info.file.response.msg);
+            const temp = this.state.value1;
+            temp.push(info.file.response.msg);
+            this.setState({
+              value1: temp,
+            });
           }
         }
       },
@@ -50,19 +59,21 @@ export default class Order extends Component {
       onChange: (info) => {
         if (info.file.response !== undefined) {
           if (info.file.response.code === 0) {
+            const temp1 = this.state.value1;
+            const temp2 = this.state.value2;
+            temp1.push(info.file.response.data.error);
+            temp2.push(info.file.response.data.log);
+
             this.setState({
-              value1: this.state.value1,
-              value2: info.file.response.data,
+              value1: temp1,
+              value2: temp2,
             });
-            // message.success(info.file.response.msg);
-            if (info.file.response.data.records.length > 0) {
-              message.success(info.file.response.records);
-            }
-            if (info.file.response.data.errors.length > 0) {
-              message.error(info.file.response.data.errors);
-            }
           } else {
-            message.error(info.file.response.msg);
+            const temp = this.state.value1;
+            temp.push(info.file.response.msg);
+            this.setState({
+              value1: temp,
+            });
           }
         }
       },
@@ -85,48 +96,32 @@ export default class Order extends Component {
             {lan.下载样例}
           </a>
         </div>
-        {
-          /*
-          <div style={{ marginTop: 20 }}>
+
+        <div style={{ marginTop: 20 }}>
           {
-            (Array.isArray(this.state.value1.records) && Array.isArray(this.state.value1.errors)) ?
-              <div className={style.reason1}>
-                <div>{lan.批量上传物流原因退款}</div>
-                <div>
-                  {
-                    this.state.value1.records.map(v => <div>{v}</div>)
-                  }
-                </div>
-                <div>
-                  {
-                    this.state.value1.errors.map(v => <div>{v}</div>)
-                  }
-                </div>
+
+            <div className={style.reason1}>
+              <div style={{ color: 'red' }}>{lan.错误信息}</div>
+              <div>
+                {
+                  this.state.value1.map(v => <div>{v}</div>)
+                }
               </div>
-                : null
+            </div>
           }
         </div>
         <div>
           {
-            (Array.isArray(this.state.value2.records) && Array.isArray(this.state.value2.errors)) ?
-              <div className={style.reason2}>
-                <div>{lan.批量上传投诉订单退款}</div>
-                <div>
-                  {
-                    this.state.value2.records.map(v => <div>{v}</div>)
-                  }
-                </div>
-                <div>
-                  {
-                    this.state.value2.errors.map(v => <div>{v}</div>)
-                  }
-                </div>
+            <div className={style.reason2}>
+              <div style={{ color: 'blue' }}>{lan.正确信息}</div>
+              <div>
+                {
+                  this.state.value2.map(v => <div>{v}</div>)
+                }
               </div>
-                : null
+            </div>
           }
         </div>
-           */
-        }
       </div>
     );
   }
