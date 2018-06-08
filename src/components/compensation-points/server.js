184dbe10257b99fc54447ad47df50df61d1c3b95
zhangcaochao <zhangcaochao30@gmail.com>
2018-06-08 17:29:38 +0800

    716

--- src/components/compensation-points/server.js
+++ src/components/compensation-points/server.js
@@ -29,14 +29,17 @@ export const pointRewardListSer  =action=>{
 
 export const addPointRewardHandleSer =action=>{
   const {props} = action
-  const keys = ["site_from","country_id","is_cod", "orderStatus", "point_id", "point_name"]
+  debugger;
+  const keys = ["site_from","country_id","is_cod", "orderStatus", "point_id", "point_name",'parent_id','point_type_id']
   const temp = assign({},props,{
     site_from:props.siteFrom2.join(','),
     country_id:props.country2.join(','),
     is_cod:props.COD_status2,
     orderStatus:props.order_status2,
-    point_id:props.selectedRows2.map(v=>v.point_type_id).join(','),
+    point_id:props.selectedRows2.map(v=>v.point).join(','),
     point_name:props.selectedRows2.map(v=>v.type_name).join(','),
+    parent_id:props.selectedRows2.map(v=>v.parent_id).join(','),
+    point_type_id:props.selectedRows2.map(v=>v.point_type_id).join(',')
   })
 
   return fetch('/Order/addPointRewardHandle',{
@@ -53,8 +56,10 @@ export const editPointRewardHandleSer =action=>{
     country_id:props.country3.join(','),
     is_cod:props.COD_status3,
     orderStatus:props.order_status3,
-    point_id:props.selectedRows3.map(v=>v.point_type_id).join(','),
+    point_id:props.selectedRows3.map(v=>v.point).join(','),
     point_name:props.selectedRows3.map(v=>v.type_name).join(','),
+    parent_id:props.selectedRows3.map(v=>v.parent_id).join(','),
+    point_type_id:props.selectedRows3.map(v=>v.point_type_id).join(',')
   })
 
   return fetch('/Order/editPointRewardHandle',{
@@ -71,8 +76,10 @@ export const copyPointRewardHandleSer=action=>{
     country_id:props.country4.join(','),
     is_cod:props.COD_status4,
     orderStatus:props.order_status4,
-    point_id:props.selectedRows4.map(v=>v.point_type_id).join(','),
+    point_id:props.selectedRows4.map(v=>v.point).join(','),
     point_name:props.selectedRows4.map(v=>v.type_name).join(','),
+    parent_id:props.selectedRows4.map(v=>v.parent_id).join(','),
+    point_type_id:props.selectedRows4.map(v=>v.point_type_id).join(',')
   })
 
   return fetch('/Order/copyPointRewardHandle',{
